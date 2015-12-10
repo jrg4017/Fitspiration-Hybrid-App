88a059309fad8f1718c02b1ca25baf4a720335b8
jrg4017 <jrg4017@rit.edu>
2015-12-10 15:57:22 -0500

    css style rules changes and updates on appropiate pages

--- fitspiration/www/js/controllers.js
+++ fitspiration/www/js/controllers.js
@@ -61,6 +61,10 @@ angular.module('fitspiration.controllers', [])
 	$scope.addItem = function() {
 		$state.go('upload');
 	}
+	
+	$scope.upload = function(){
+		$state.go('upload');
+	}
   };
   
   /*checks for new items every 20 seconds and loads it into the newItems array */
